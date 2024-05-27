5ffeea1e0729c89cd4f5a1c04014fb8909b61636
sagar1712 <sagars536335@gmail.com>
2024-05-28 01:04:32 +0530

    Feat: Created create-server-modal triggered from sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ModalProvider } from '@/components/providers/modal-provider';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { cn } from '@/lib/utils';
 import { ClerkProvider } from '@clerk/nextjs';
@@ -26,6 +27,7 @@ export default function RootLayout({
             defaultTheme="dark"
             storageKey="discord-clone-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
--- src/components/navigation/navigation-action.tsx
+++ src/components/navigation/navigation-action.tsx
@@ -1,12 +1,17 @@
 'use client';
+import { useModal } from '@/hooks/use-modal-store';
 import { Plus } from 'lucide-react';
 import { ActionTooltip } from '../action-tooltip';
 
 const NavigationAction = () => {
+  const { onOpen } = useModal();
   return (
     <div>
       <ActionTooltip side="right" align="center" label="Add a Server">
-        <button className="group flex items-center">
+        <button
+          onClick={() => onOpen('createServer')}
+          className="group flex items-center"
+        >
           <div className="mx-3 flex h-[48px] w-[48px] items-center justify-center overflow-hidden rounded-[24px] bg-background transition-all group-hover:rounded-[16px] group-hover:bg-emerald-500 dark:bg-neutral-700">
             <Plus
               className="text-emerald-500 transition group-hover:text-white"
