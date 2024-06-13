ef465d402e27abb552980208b809c130112633ce
sagar1712 <sagars536335@gmail.com>
2024-06-14 02:11:06 +0530

    feat: rendered chat messages in channel conversation

---
+++ src/components/chat/chat-welcome.tsx
@@ -0,0 +1,28 @@
+import { Hash } from 'lucide-react';
+
+interface ChatWelcomeProps {
+  type: 'channel' | 'conversation';
+  name: string;
+}
+
+const ChatWelcome = ({ type, name }: ChatWelcomeProps) => {
+  return (
+    <div className="mb-4 space-y-2 px-4">
+      {type === 'channel' && (
+        <div className="flex h-[75px] w-[75px] items-center justify-center rounded-full bg-zinc-500 dark:bg-zinc-200">
+          <Hash className="h-12 w-12 text-zinc-100 dark:text-zinc-700" />
+        </div>
+      )}
+      <p className="text-xl font-bold md:text-3xl">
+        {type === 'channel' ? 'Welcome to #' : ' '}
+        {name}
+      </p>
+      <p className="text-sm text-zinc-600 dark:text-zinc-400">
+        {type === 'channel'
+          ? `This is the start of the #${name} channel.`
+          : `This is the start of your conversation with ${name}`}
+      </p>
+    </div>
+  );
+};
+export default ChatWelcome;
